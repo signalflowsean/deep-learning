4291adcb4228982ae778512ad13bec6c31bc8deb
Sean Nealon <signalflowsean@gmail.com>
2019-03-03 12:15:53 -0500

    neural net to predict which customers leave bank works

--- Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/Artificial_Neural_Networks/ann.py
+++ Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/Artificial_Neural_Networks/ann.py
@@ -50,4 +50,44 @@ X_test = sc.transform(X_test)
 
 # Part 2 - Now let's make the ANN!
 # Importing the Kera libraries and packages
-import keras
+import keras 
+from keras.models import Sequential
+from keras.layers import Dense
+#from keras.backend.tensorflow_backend import set_session
+
+
+
+#1. Randomly initialize the weights to small numbers close to (but not 0)
+#2. Input the first observation of your dataset in the input layer, each feature in one input node
+#3. Forward-Propagation
+    # The neuron applies the activation function to this sum. 
+    # The closer the activation function value is to 1, tge more the neuron passes on the signal.
+#4. Compare the predicted result to the actual result. Measure generated error. 
+#5. Back-Propagation. 
+    #The error is back-propagated. Update the weights accortding to how much they are responsible for the error. 
+    #The learning rate decides by how much we update the weights
+#6. Reapeat steps 1 to 5 and update the weights after each observation
+    #Or only after a batch observations 
+#7. This makes an epoch. Redo more epochs.  
+    
+#Paramater tuning    
+
+#Initialize the ANN
+classifier = Sequential() 
+
+#Adding the input layer and the first hidden layer
+classifier.add(Dense(output_dim = 6, init='uniform', activation='relu', input_dim = 11))
+
+#Adding the second hidden layer - already made the first input layer (don't need input_dim param)
+classifier.add(Dense(output_dim = 6, init='uniform', activation='relu'))
+
+#Adding the output layer
+classifier.add(Dense(output_dim = 1, init='uniform', activation='sigmoid'))
+
+#Compiling the ANN - using gradient descent
+#For logistic loss function is logarithmic loss
+#Adam a method for stochastic optimization
+classifier.compile(optimizer = 'adam', loss= 'binary_crossentropy',metrics = ['accuracy'])
+
+#Fitting the ANN to the Training set
+classifier.fit(X_train, y_train, batch_size=10, nb_epoch=100)
