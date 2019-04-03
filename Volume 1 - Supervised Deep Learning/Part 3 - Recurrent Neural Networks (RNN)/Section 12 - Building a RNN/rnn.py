ef2f44a8a6312d4eb51154a75cf0029e723d4c02
Sean Nealon <signalflowsean@gmail.com>
2019-04-03 16:43:47 -0400

    import libraries

--- Volume 1 - Supervised Deep Learning/Part 3 - Recurrent Neural Networks (RNN)/Section 12 - Building a RNN/rnn.py
+++ Volume 1 - Supervised Deep Learning/Part 3 - Recurrent Neural Networks (RNN)/Section 12 - Building a RNN/rnn.py
@@ -9,5 +9,13 @@ Created on Wed Apr  3 16:28:33 2019
 #Dropout is a technique where randomly selected neurons are ignored during training. They are “dropped-out” randomly. This means that their contribution to the activation of downstream neurons is temporally removed on the forward pass and any weight updates are not applied to the neuron on the backward pass.
 
 # Part 1 - Data Preprocesing
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the training set
+dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
+training_set = dataset_train.iloc[:, ]
+
 # Part 2 - Building the RNN
 # Part 3 - Making the predictions and vizualizing the results
