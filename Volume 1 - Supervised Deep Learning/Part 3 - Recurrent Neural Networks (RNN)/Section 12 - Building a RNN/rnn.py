5e92b13515a6ebd7e7bab671e7d25524179d12e6
Sean Nealon <signalflowsean@gmail.com>
2019-04-03 16:39:18 -0400

    training and testdata and basic outline of process in rnn.py

---
+++ Volume 1 - Supervised Deep Learning/Part 3 - Recurrent Neural Networks (RNN)/Section 12 - Building a RNN/rnn.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  3 16:28:33 2019
+
+@author: signalflowsean
+"""
+
+#Dropout is a technique where randomly selected neurons are ignored during training. They are “dropped-out” randomly. This means that their contribution to the activation of downstream neurons is temporally removed on the forward pass and any weight updates are not applied to the neuron on the backward pass.
+
+# Part 1 - Data Preprocesing
+# Part 2 - Building the RNN
+# Part 3 - Making the predictions and vizualizing the results
